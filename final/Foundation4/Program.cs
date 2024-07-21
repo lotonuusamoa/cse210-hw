8547ee1d970629322c26f25ef81959671020421e
lotonuusamoa <andre@lotonuu.com>
2024-07-20 18:09:14 -1100

    review foundation 2

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities =  new List<Activity>();
+        activities.Add(new RunningActivity("25 June 2024",5.7,30));
+        activities.Add(new CyclingActivity("25 May 2024",7.6,30));
+        activities.Add(new Swimming("23 April 2024",36,30));
+
+        int index = 1;
+        foreach(Activity activity in activities)
+        {
+            Console.WriteLine($"\nACTIVITY N°{index}:\n{activity.GetSummary()}");
+            index++;
+        }
+        Console.WriteLine();    
     }
 }
